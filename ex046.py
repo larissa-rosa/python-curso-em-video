8280a6e245918a3bf2352f427afe1aa9e69bd46b
larissa-rosa <larissa_rosa7@hotmail.com>
2022-08-19 18:10:11 -0300

    Adição do arquivo do exercício 046 atualizado

--- ex046.py
+++ ex046.py
@@ -1,8 +1,9 @@
 from time import sleep
+
 print('Contagem regressiva para os fogos de artifício!')
 
-for c in range(10, 0, -1):
+for c in range(10, -1, -1):
     print(c)
     sleep(1)
-print('Já!')
+print('Boom!')
 
